bc14f95a3fa97aabf637775b7a750f2236e653fd
CrystalPandah <44172504+CrystalPandah@users.noreply.github.com>
2020-02-04 00:44:46 +0000

    Lab 3 part 2,3, 3a
    Added a circl;e that moved wiht delta time, then changed it to bounce off of each side of the window, making sure this still happens when the window is resized

--- Week3/CMP105App/Level.cpp
+++ Week3/CMP105App/Level.cpp
@@ -6,7 +6,14 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	input = in;
 
 	// initialise game objects
+	//Moving circle defined
+	circle.setRadius(15);
+	circle.setPosition(300, 300);
+	circle.setFillColor(sf::Color::Green);
+	circle.setOutlineColor(sf::Color::Magenta);
+	circle.setOutlineThickness(2.f);
 
+	speed = 150.0f;
 }
 
 Level::~Level()
@@ -23,13 +30,42 @@ void Level::handleInput(float dt)
 // Update game objects
 void Level::update(float dt)
 {
+	window->getSize().x;
+	window->getSize().y;
+
+	circle.getRadius();
+	circle.getRadius() * 2;
+
+	if (circle.getPosition().x + (circle.getRadius()*2) >= (window->getSize().x))
+	{
+		// Move circle so it is not past the edge
+		circle.setPosition(window->getSize().x-(circle.getRadius()*2), circle.getPosition().y);
+		
+		// Reverse speed
+		speed *= -1;
+	}
+	else if (circle.getPosition().x <= 0)
+	{
+		// Move circle so it is not past the edge
+		circle.setPosition(0, circle.getPosition().y);
+		
+		// Reverse the speed
+		speed *= -1;
+	}
+
 	
+	
+
+	//Update/move circle
+	circle.move(speed*dt, 0);
 }
 
 // Render level
 void Level::render()
 {
 	beginDraw();
+
+	window->draw(circle);
 
 	endDraw();
 }
--- Week3/CMP105App/Level.h
+++ Week3/CMP105App/Level.h
@@ -23,4 +23,10 @@ private:
 	// Default variables for level class.
 	sf::RenderWindow* window;
 	Input* input;
+
+	//Moving circle vars
+	sf::CircleShape circle;
+	float speed;
+
+
 };
